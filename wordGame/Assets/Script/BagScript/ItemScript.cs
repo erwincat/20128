3ba196d8b71283ae7abf4658fa90e7a4d9dcaeb9
wuchangming <wuchangming@163.com>
2021-02-04 13:21:35 +0800

    bag

---
+++ wordGame/Assets/Script/BagScript/ItemScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item",menuName = "Bag/New Item")]
+public class ItemScript : ScriptableObject
+{
+    public string ItemName;
+    public Sprite ItemICon;
+    public int ItemNumber;
+    [TextArea]
+    public string ItemInfo;
+
+    public bool isCuandai;//是否可装备
+}
